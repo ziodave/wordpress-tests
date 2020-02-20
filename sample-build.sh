1debdb6a5b95c413cef04d619e06aca1b258c765
David Riccitelli <david@wordlift.io>
2020-02-20 16:55:58 +0100

    initial commit.

---
+++ sample-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PHP_VERSION=${PHP_VERSION:-7.2}
+PHPUNIT_VERSION=${PHPUNIT_VERSION:-4.8}
+WORDPRESS_VERSION=${WORDPRESS_VERSION:-5.3}
+TAG=wordpress-tests:php${PHP_VERSION}-phpunit${PHPUNIT_VERSION}-wordpress${WORDPRESS_VERSION}
+
+echo "Building $TAG..."
+
+docker build --build-arg PHPUNIT_VERSION=$PHPUNIT_VERSION \
+  --build-arg WORDPRESS_VERSION=$WORDPRESS_VERSION \
+  --build-arg PHP_VERSION=$PHP_VERSION \
+  -t $TAG .
