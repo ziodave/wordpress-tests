e548989973ba63789929d91a99a085aac0e3c200
David Riccitelli <david@wordlift.io>
2020-12-03 12:44:02 +0100

    update

--- .build/create-actions.sh
+++ .build/create-actions.sh
@@ -11,10 +11,21 @@ matrix=(
     '7.3|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5'
 )
 
+phpunit_version=4.8
+release=v0.0.1
+
 for fields in ${matrix[@]}; do
     IFS=$'|' read -r php_version fields <<< "$fields"
     IFS=$',' read -r -a wordpress_versions <<< "$fields"
     for wordpress_version in ${wordpress_versions[@]}; do
-        PHP_VERSION=$php_version WORDPRESS_VERSION=$wordpress_version sh -c ./sample-build.sh
+        image_tag=php${php_version}-phpunit${phpunit_version}-wordpress${wordpress_version}
+        image_name=docker://ziodave/wordpress-tests:$image_tag
+        git_tag=$image_tag-$release
+        sed -e "s/__IMAGE_NAME__/${image_name//\//\\/}/g" action.template.yml > ../action.yml
+        git commit -m "Create action.yml for $image_name" ../action.yml
+        git tag -d $git_tag
+        git push origin :$git_tag
+        git tag -a -m "Tagging $git_tag" $git_tag
+        git push --follow-tags
     done
 done
--- .docker/build-all.sh
+++ .docker/build-all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 matrix=(
-    '5.3|4.4'
     '5.4|4.4'
     '5.5|4.4'
     '5.6|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5'
@@ -11,19 +10,10 @@ matrix=(
     '7.3|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5'
 )
 
-phpunit_version=4.8
-release=v0.0.1
-
 for fields in ${matrix[@]}; do
     IFS=$'|' read -r php_version fields <<< "$fields"
     IFS=$',' read -r -a wordpress_versions <<< "$fields"
     for wordpress_version in ${wordpress_versions[@]}; do
-        image_tag=php${php_version}-phpunit${phpunit_version}-wordpress${wordpress_version}
-        image_name=docker://ziodave/wordpress-tests:$image_tag
-        git_tag=$image_name-$release
-        sed -e "s/__IMAGE_NAME__/${image_name//\//\\/}/g" action.template.yml
-        git commit -m "Create action.yml for $image_name"
-        git tag -a -m "Tagging $git_tag" git_tag
-        git push --follow-tags
+        PHP_VERSION=$php_version WORDPRESS_VERSION=$wordpress_version sh -c ./build.sh
     done
 done
