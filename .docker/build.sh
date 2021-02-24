6af4253afcb532f6ebc570bd81a698bff863f2f3
David Riccitelli <david@wordlift.io>
2021-02-24 11:53:19 +0100

    add support for xdebug and phpunit coverage

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
+
+PHP_CODE_COVERAGE=${PHP_CODE_COVERAGE:-2}
 PHP_VERSION=${PHP_VERSION:-7.2}
 PHPUNIT_VERSION=${PHPUNIT_VERSION:-4.8}
 WORDPRESS_VERSION=${WORDPRESS_VERSION:-5.3}
@@ -7,7 +11,9 @@ TAG=wordpress-tests:php${PHP_VERSION}-phpunit${PHPUNIT_VERSION}-wordpress${WORDP
 
 echo "Building $TAG..."
 
-docker build --build-arg PHPUNIT_VERSION=$PHPUNIT_VERSION \
+docker build \
+  --build-arg PHP_CODE_COVERAGE=$PHP_CODE_COVERAGE \
+  --build-arg PHPUNIT_VERSION=$PHPUNIT_VERSION \
   --build-arg WORDPRESS_VERSION=$WORDPRESS_VERSION \
   --build-arg PHP_VERSION=$PHP_VERSION \
   -t ziodave/$TAG . \
