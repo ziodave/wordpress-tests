1e8ff536039cad8a5f0129b6f7d18b5001176009
David Riccitelli <david@wordlift.io>
2021-11-15 09:11:17 +0200

    add xdebug support based on PHP version

--- .docker/build-all.sh
+++ .docker/build-all.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 matrix=(
-    '5.4|4.4|4.8|2'
-    '5.5|4.4|4.8|2'
-    '5.6|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|4.8|2'
-    '7.0|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|4.8|2'
-    '7.1|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|7.5|6'
-    '7.2|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|7.5|6'
-    '7.3|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|7.5|6'
+    '5.4|4.4|4.8|2|2.4.1'
+    '5.5|4.4|4.8|2|2.5.5'
+    '5.6|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|4.8|2|2.5.5'
+    '7.0|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|4.8|2|2.6.1'
+    '7.1|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|7.5|6|2.9.8'
+    '7.2|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|7.5|6|3.1.1'
+    '7.3|4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6|7.5|6|3.1.1'
 )
 
 for fields in ${matrix[@]}; do
-    IFS=$'|' read -r php_version fields phpunit_version php_code_coverage<<< "$fields"
+    IFS=$'|' read -r php_version fields phpunit_version php_code_coverage xdebug_version<<< "$fields"
     IFS=$',' read -r -a wordpress_versions <<< "$fields"
     for wordpress_version in ${wordpress_versions[@]}; do
-        PHP_VERSION=$php_version WORDPRESS_VERSION=$wordpress_version PHPUNIT_VERSION=$phpunit_version PHP_CODE_COVERAGE=$php_code_coverage sh -c ./build.sh
+        PHP_VERSION=$php_version WORDPRESS_VERSION=$wordpress_version PHPUNIT_VERSION=$phpunit_version PHP_CODE_COVERAGE=$php_code_coverage XDEBUG_VERSION=$xdebug_version sh -c ./build.sh
     done
 done
